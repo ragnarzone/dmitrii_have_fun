b02dcb02ffa81c2979e3f34cde966ee807d0cba8
Dmitrii <ragnarzone@gmail.com>
2023-02-02 12:50:58 +0100

    added C-sharp collections example

---
+++ collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class Program{
+    public static void Main(){
+        ListTest();
+    }
+
+    private static void ListTest(){
+        var salmons = new List<string>();
+        salmons.Add("chinook");
+        salmons.Add("coho");
+        salmons.Add("pink");
+        salmons.Add("sockeye");
+
+        salmons.FaorEach(
+            salmon => Console.Write(salmon + " \n"));
+    }
+}
+
